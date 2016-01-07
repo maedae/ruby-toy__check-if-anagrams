ed9be0d24e7456827fb7c94a29d2994411726f02
maedae <meagan.jungman@gmail.com>
2016-01-07 20:28:00 +0000

    done. also added checking if it's the same word

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,10 @@
 def anagrams?(str1, str2)
    if
+    str1.downcase == str2.downcase
+    false
+elsif 
     str1.downcase.split("").sort == str2.downcase.split("").sort
-    true
+   true
 else 
     false
 end
