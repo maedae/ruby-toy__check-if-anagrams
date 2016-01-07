0deeea8546cefdb621ece1f7039037b7b43582eb
maedae <meagan.jungman@gmail.com>
2016-01-07 21:20:32 +0000

    cleaned :]

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,11 +1,9 @@
 def anagrams?(str1, str2)
-   if
-    str1.downcase == str2.downcase
-    false
-elsif 
-    str1.downcase.split("").sort == str2.downcase.split("").sort
-   true
-else 
-    false
-end
+    if str1.downcase == str2.downcase
+        false
+    elsif str1.downcase.split("").sort == str2.downcase.split("").sort
+        true
+    else 
+        false
+    end
 end
